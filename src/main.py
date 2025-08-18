b377c1ff17758200859029c6fd47e61211088fea
7minutech <elijohnson1107@gmail.com>
2025-08-18 11:18:17 -0500

    feat: copy_to_destination & helper copy_dir_contents methods

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 from textnode import TextNode, TextType
+import os, shutil
 
 def main():
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    copy_to_destination("static", "public")
+
+def copy_to_destination(source_path, destination_path):
+    shutil.rmtree(destination_path)
+    os.mkdir(destination_path)
+    copy_dir_contents(source_path, destination_path)
+    pass
+
+def copy_dir_contents(dir_path, destination):
+    if os.path.exists(dir_path):
+        dir_contents = os.listdir(dir_path)
+        for item in dir_contents:
+            item_path = os.path.join(dir_path, item)
+            if os.path.isdir(item_path):
+                new_dir_path = os.path.join(destination, item)
+                os.mkdir(new_dir_path)
+                copy_dir_contents(item_path, new_dir_path)
+            else:
+                shutil.copy(item_path, destination)
+
 
 main()
